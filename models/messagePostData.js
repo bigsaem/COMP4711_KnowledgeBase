ef0e577965a174c0e8d1383daf7a2e32c2167af5
SmallYellowFace <jctheathlete@gmail.com>
2020-03-29 14:58:25 -0700

    change

--- models/messagePostData.js
+++ models/messagePostData.js
@@ -1,2 +1,17 @@
 let db = require('../db/db');
 
+function addMessagePost(data) {
+    let sql = `INSERT INTO messagepost (topic, subject, postdetail, "timestamp","userid") VALUES('${data.topic}', '${data.subject}', '${data.postdetail}', '${data.timestamp}','${data.userid}')`;
+    return db.query(sql);
+}
+
+function getMessagePost(userid) {
+    let sql = `SELECT * FROM knowledge_schema.messagepost where userid = '${userid}'`;
+    return db.query(sql);
+}
+
+
+module.exports = {
+    addPost : addMessagePost,
+    getPost : getMessagePost,
+}
