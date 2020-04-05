e96b14f485b50b6969471253342328e7e6b531c7
Derek Leung <derek@localhost.localdomain>
2020-04-04 23:59:44 -0700

    reverting local db change

--- db/db.js
+++ db/db.js
@@ -1,19 +1,19 @@
 //connect to postgres
 const Pool = require("pg").Pool;
 
-// const pool = new Pool({
-//         host: 'ec2-54-157-78-113.compute-1.amazonaws.com',
-//         user: 'hjxurrvtkidtez',
-//         database: 'ddka4gdca2tlbm',
-//         password: '9ca927314ab660622d9389731e08b994eb893428ff547d08d2b90deb90b70416',
-//         port: 5432,
-//         ssl: true
-// });
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  database: "c4711",
-  password: "pgpass",
-  port: 5432
+  host: "ec2-54-157-78-113.compute-1.amazonaws.com",
+  user: "hjxurrvtkidtez",
+  database: "ddka4gdca2tlbm",
+  password: "9ca927314ab660622d9389731e08b994eb893428ff547d08d2b90deb90b70416",
+  port: 5432,
+  ssl: true
 });
+// const pool = new Pool({
+//   host: "localhost",
+//   user: "postgres",
+//   database: "c4711",
+//   password: "pgpass",
+//   port: 5432
+// });
 module.exports = pool;
