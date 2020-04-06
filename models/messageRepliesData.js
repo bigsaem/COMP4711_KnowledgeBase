4942678c8d437231f0466d2b1e7e2ed0ecdf2492
Sammy <bigsaem@gmail.com>
2020-04-05 23:41:30 -0700

    sql injection solved

--- models/messageRepliesData.js
+++ models/messageRepliesData.js
@@ -9,13 +9,14 @@ let postMessage = (data) => {
           INSERT INTO ${table}
               (subject, content, "timestamp", "to", "from")
           VALUES
-             ('${data.subject}',
-              $1, 
+             ($1,
+              $2, 
               to_timestamp(${data.timestamp / 1000}),
               '${data.to}',
               '${data.from}');
       `
     ,[
+      data.subject,
       data.content
     ])
       .then((data) => {
@@ -36,10 +37,10 @@ let getConversation = (data) => {
           ON ${table}.from = profile.userid
           WHERE ((${table}.to = ${data.to} AND ${table}.from = ${data.userid})
           OR (${table}.to = ${data.userid} AND ${table}.from = ${data.to}))
-          AND subject = '${data.subject}'
+          AND subject = $1
           ORDER BY ${table}.timestamp ASC
       `
-    )
+    , [data.subject])
       .then((data) => {
         resolve(data.rows);
       })
