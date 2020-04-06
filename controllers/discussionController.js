fb1c13853800fe4dded79ec43b81cb03fa9c01fa
Justin Cervantes <cervantes.jfa@gmail.com>
2020-04-05 23:39:14 -0700

    Adding the proper form target

--- controllers/discussionController.js
+++ controllers/discussionController.js
@@ -89,7 +89,7 @@ exports.getAllReplies = (req, res, next) => {
 exports.addComment = (req, res, next) => {
   let postid = req.params.postid;
   let replydetail = req.body.replydetail;
-  let timestamp = req.body.timestamp;
+  let timestamp = Date.now();
   let userid = req.body.userid; //not sure how this is passed yet
 
   replyData
