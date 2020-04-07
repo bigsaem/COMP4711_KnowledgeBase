eb2c0e3d4fb0753bf744ec9deb068dd60782624a
Keunsaem Lee <35913956+bigsaem@users.noreply.github.com>
2020-04-07 10:34:30 -0700

    Revert "bug fixed, removed print statements and removed routes.js"

--- public/js/userprofile.js
+++ public/js/userprofile.js
@@ -2,5 +2,6 @@ let userprofileSubmit = document.getElementById("userprofile-like-link");
 userprofileSubmit.onclick = function() {
     document.getElementById("userprofilelikeform").onclick() = function() {
         window.getElementById("userprofilelikeform").submit();
+        console.log("Hello world")
     };
 }
